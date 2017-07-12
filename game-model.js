28cee8313adafae5d8a079f03f02d734e73d3a4c
Liz Jewell <eghjewell@gmail.com>
2017-07-12 09:42:14 -0400

    core HTML, CSS, and beginning JS

---
+++ game-model.js
@@ -0,0 +1,72 @@
+var content = [
+  {
+    question: 'Question 1',
+    answers: {
+      right: 'Answer 1',
+      wrongAnswers: ['Answer 2', 'Answer 3','Answer 4']
+    }
+  },
+  {
+    question: 'Question 2',
+    answers: {
+      right: 'Answer 1',
+      wrongAnswers: ['Answer 2', 'Answer 3','Answer 4']
+    }
+  },
+  {
+    question: 'Question 3',
+    answers: {
+      right: 'Answer 1',
+      wrongAnswers: ['Answer 2', 'Answer 3','Answer 4']
+    }
+  },
+  {
+    question: 'Question 4',
+    answers: {
+      right: 'Answer 1',
+      wrongAnswers: ['Answer 2', 'Answer 3','Answer 4']
+    }
+  },
+  {
+    question: 'Question 5',
+    answers: {
+      right: 'Answer 1',
+      wrongAnswers: ['Answer 2', 'Answer 3','Answer 4']
+    }
+  },
+  {
+    question: 'Question 6',
+    answers: {
+      right: 'Answer 1',
+      wrongAnswers: ['Answer 2', 'Answer 3','Answer 4']
+    }
+  },
+  {
+    question: 'Question 7',
+    answers: {
+      right: 'Answer 1',
+      wrongAnswers: ['Answer 2', 'Answer 3','Answer 4']
+    }
+  },
+  {
+    question: 'Question 8',
+    answers: {
+      right: 'Answer 1',
+      wrongAnswers: ['Answer 2', 'Answer 3','Answer 4']
+    }
+  },
+  {
+    question: 'Question 9',
+    answers: {
+      right: 'Answer 1',
+      wrongAnswers: ['Answer 2', 'Answer 3','Answer 4']
+    }
+  },
+  {
+    question: 'Question 10',
+    answers: {
+      right: 'Answer 1',
+      wrongAnswers: ['Answer 2', 'Answer 3','Answer 4']
+    }
+  }
+]
